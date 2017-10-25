97d88b65e8f1a78a453dd41bcf2fc81f8fd28990
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-25 14:35:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,37 +7,38 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do 
+  get '/' do
     erb :home
   end
 
-  # Render the sign-up form view
+  # render the sign-up form view
   get '/registrations/signup' do
     erb :'/registrations/signup'
   end
 
-  # gets the new user's info from the params hash, creates a new user, signs them in, and then redirects them 
+  # gets the new user's info from the params hash, creates a new user, signs them in, and then redirects them
   post '/registrations' do
-    
+
     redirect '/users/home'
   end
 
+  # render the login form
   get '/sessions/login' do
     erb :'sessions/login'
   end
 
   post '/sessions' do
-    
+
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
+  get '/sessions/logout' do
 
     redirect '/'
   end
 
   get '/users/home' do
-   
+
     erb :'/users/home'
   end
 
