ebcee283193959ab311aef2095a4cd31009f359d
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-25 15:03:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,40 +11,34 @@ class ApplicationController < Sinatra::Base
     erb :home
   end
 
-  # render the sign-up form view
   get '/registrations/signup' do
     erb :'/registrations/signup'
   end
 
-  # gets the new user's info from the params hash, creates a new user, signs them in, and then redirects them
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:id] = @user.id
+
     redirect '/users/home'
   end
 
-  # render the login form
   get '/sessions/login' do
     erb :'sessions/login'
   end
 
-  # grabs the user's info from the params hash, looks to match that info against the existing entries in the user database, and, if a matching entry is found, signs the user in
   post '/sessions' do
-
+    @user = User.find_by(email: params[:email], password: params[:password])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
-  # log the user out by clearing the session hash
   get '/sessions/logout' do
-
+    session.clear
     redirect '/'
   end
 
-  # render the user's homepage view
   get '/users/home' do
-
+   @user = User.find(session[:id])
     erb :'/users/home'
   end
-
-end
